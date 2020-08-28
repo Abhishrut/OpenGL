2f26ff83c6fc9f5fa6a7f162202c3513c96a5bd3
Abhishrut <abhishrutk37@gmail.com>
2020-08-29 03:33:11 +0530

    Creating a TestMenu
    A testmenu for all our tests

--- src/Application.cpp
+++ src/Application.cpp
@@ -20,86 +20,105 @@
 #include"imgui/imgui_impl_opengl3.h"
 #include"tests/TestClearColor.h"
 
+
 int main(void)
 {
 	{
-	GLFWwindow* window;
+		GLFWwindow* window;
 
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
+		/* Initialize the library */
+		if (!glfwInit())
+			return -1;
 
-	/*Changing OpenGL mode from compatiblility to Core Mode*/
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4); //version Major.minor version 3.3 //not working for me :(
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
+		/*Changing OpenGL mode from compatiblility to Core Mode*/
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4); //version Major.minor version 3.3 //not working for me :(
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		/* Create a windowed mode window and its OpenGL context */
+		window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
+		if (!window)
+		{
+			glfwTerminate();
+			return -1;
+		}
 
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
-	//glfwSwapInterval(1);   //make the refresh rate = refresh rate of the monitor OR sync with a V sync in my case this is a default 
+		/* Make the window's context current */
+		glfwMakeContextCurrent(window);
+		//glfwSwapInterval(1);   //make the refresh rate = refresh rate of the monitor OR sync with a V sync in my case this is a default 
 
 
-	if (glewInit() != GLEW_OK) {
-		std::cout << "Error" << std::endl;
-	}
-	std::cout << glGetString(GL_VERSION) << std::endl;
+		if (glewInit() != GLEW_OK) {
+			std::cout << "Error" << std::endl;
+		}
+		std::cout << glGetString(GL_VERSION) << std::endl;
 
-	float positions[] = {
-		-50.f,-50.f, 0.0f, 0.0f,//0 botton left
-		 50.f,-50.f, 1.0f, 0.0f,//1 bottom right
-		 50.f, 50.f, 1.0f, 1.0f,//2 top right
-		-50.f, 50.f, 0.0f, 1.0f //3 top left
-	};
+		float positions[] = {
+			-50.f,-50.f, 0.0f, 0.0f,//0 botton left
+			 50.f,-50.f, 1.0f, 0.0f,//1 bottom right
+			 50.f, 50.f, 1.0f, 1.0f,//2 top right
+			-50.f, 50.f, 0.0f, 1.0f //3 top left
+		};
 
-	unsigned int indices[] = {
-		0,1,2,
-		0,2,3
-	};
+		unsigned int indices[] = {
+			0,1,2,
+			0,2,3
+		};
 	
-	GLCall(glEnable(GL_BLEND));
-	GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+		GLCall(glEnable(GL_BLEND));
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-	Renderer renderer;
+		Renderer renderer;
 
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	ImGui::StyleColorsDark();
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init((char *)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
+		ImGuiIO& io = ImGui::GetIO(); (void)io;
+		ImGui::StyleColorsDark();
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		ImGui_ImplOpenGL3_Init((char *)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
 
-	test::TestClearColor test;
+		test::Test* currentTest=nullptr;
+		test::TestMenu* testMenu = new test::TestMenu(currentTest);
+		currentTest = testMenu;
+	
+		testMenu->RegisterTest<test::TestClearColor>("Clear Color");
 
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{	//we are doing this every frame
-		/* Render here */
-		renderer.Clear();
+
+		/* Loop until the user closes the window */
+		while (!glfwWindowShouldClose(window))
+		{	//we are doing this every frame
+			/* Render here */
+			GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
+			renderer.Clear();
 		
-		test.OnUpdate(0.0f);
-		test.OnRender();
-
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
  		
-		test.OnImGuiRender();
+			if (currentTest) 
+			{
+				currentTest->OnUpdate(0.0f);
+				currentTest->OnRender();
+				ImGui::Begin("Test");
+				if (currentTest != testMenu && ImGui::Button("<-"))
+				{
+					delete currentTest;
+					currentTest = testMenu;
+				}
+				currentTest->OnImGuiRender();
+				ImGui::End();
+			}
+		
+			ImGui::Render();
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+			/* Swap front and back buffers */
+			glfwSwapBuffers(window);
 
-		ImGui::Render();
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
-		glfwPollEvents();
-	}
+			/* Poll for and process events */
+			glfwPollEvents();
+		}
+		delete currentTest;
+		if (currentTest != testMenu)
+			delete testMenu;
 	}
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplGlfw_Shutdown();
